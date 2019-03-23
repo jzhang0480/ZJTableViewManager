8457d8a857bd1af006cb4b366385bba502bc6ed1
张杰 <1074472615@qq.com>
2019-03-23 16:31:34 +0800

    Demo2适配Swift4.2，优化显示效果

--- Demo2/Demo2/ViewController.swift
+++ Demo2/Demo2/ViewController.swift
@@ -15,12 +15,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var productTableView: UITableView!
     var categoryManager: ZJTableViewManager!
     var productManager: ZJTableViewManager!
+    //是否在向上滚动
+    var isScrollUp:Bool = false
+    var lastOffsetY:CGFloat = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //初始化tableviewManager
         self.categoryManager = ZJTableViewManager(tableView: self.categoryTableView)
         self.productManager = ZJTableViewManager(tableView: self.productTableView)
+        self.productManager.delegate = self
         //假数据
         let arrCategory = ["分类1","分类2","分类3","分类4","分类5","分类6"]
         let arrProduct = ["面包", "蛋糕", "香蕉", "牛奶", "饼干", "猫粮"]
@@ -30,11 +34,11 @@ class ViewController: UIViewController {
         self.categoryManager?.add(section: categorySection)
         for category in arrCategory {
             let categoryItem = ZJTableViewItem(title: category)
-            categoryItem.isSelectionAnimate = false
+            categoryItem.isAutoDeselect = false
             categorySection.add(item: categoryItem)
             //分类的点击事件
             categoryItem.setSelectionHandler(selectHandler: {[weak self] (item) in
-                self?.productManager.tableView.scrollToRow(at: IndexPath(row: 0, section: item.indexPath.row), at: .top, animated: false)
+                self?.productManager.tableView.scrollToRow(at: IndexPath(row: 0, section: item.indexPath.row), at: .top, animated: true)
             })
         }
         
@@ -48,8 +52,22 @@ class ViewController: UIViewController {
             self.productManager.add(section: section)
             //商品列表滑动时与分类列表联动
             section.setHeaderWillDisplayHandler({[weak self] (currentSection) in
-                self?.categoryManager.tableView.selectRow(at: IndexPath(item: currentSection.index, section: 0), animated: false, scrollPosition: .middle)
+                //手动拖才和左边联动
+                if (self?.productManager.tableView.isDragging ?? false && !(self?.isScrollUp ?? true)){
+                    let currentSection = self?.productTableView.indexPathsForVisibleRows?.first?.section ?? 0
+                    self?.categoryTableView.selectRow(at: IndexPath(item: currentSection, section: 0), animated: false, scrollPosition: .middle)
+                }
             })
+            
+            section.setHeaderDidEndDisplayHandler {[weak self] (currentSection) in
+                //手动拖才和左边联动
+                if (self?.productManager.tableView.isDragging ?? false && (self?.isScrollUp ?? true)){
+                     self?.categoryTableView.selectRow(at: IndexPath(item: currentSection.index + 1, section: 0), animated: false, scrollPosition: .middle)
+                }else if (self?.productManager.tableView.isDragging ?? false && !(self?.isScrollUp ?? true)){
+                    let currentSection = self?.productTableView.indexPathsForVisibleRows?.first?.section ?? 0
+                    self?.categoryTableView.selectRow(at: IndexPath(item: currentSection, section: 0), animated: false, scrollPosition: .middle)
+                }
+            }
             //添加商品
             for product in arrProduct {
                 let item = ZJTableViewItem(title: product)
@@ -58,6 +76,8 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+    
     
     
 
@@ -69,3 +89,11 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController:ZJTableViewDelegate{
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        isScrollUp = lastOffsetY < scrollView.contentOffset.y;
+        lastOffsetY = scrollView.contentOffset.y;
+    }
+    
+    
+}
