efd91d8a9274be9032892dc10d97598c8b123ccb
Javen <1074472615@qq.com>
2019-10-25 16:39:24 +0800

    no message

--- 1.0.0Demo/test/ZJTableViewManager/Cells/ZJTextCell.swift
+++ 1.0.0Demo/test/ZJTableViewManager/Cells/ZJTextCell.swift
@@ -10,11 +10,7 @@ import UIKit
 
 open class ZJTextCell: UITableViewCell, UITextViewDelegate, ZJCellProtocol  {
     public var item: ZJTextItem!
-    
     public typealias ZJCelltemType = ZJTextItem
-    
-    
-    public typealias ZJTableViewItemType = ZJTextItem
     
     public func cellWillAppear() {
         textView.placeholder = item.placeHolder
@@ -40,8 +36,8 @@ open class ZJTextCell: UITableViewCell, UITextViewDelegate, ZJCellProtocol  {
     }
 
     public func textViewDidChange(_ textView: UITextView) {
-//        item.text = textView.text
-//        item.didChanged?(item)
+        item.text = textView.text
+        item.didChanged?(item)
     }
 
     open override func setSelected(_ selected: Bool, animated: Bool) {
